f70c985374f142a526d1fb49b8da586f2b5af127
Luka Rekhviashvili <rekhviashvili.luka125@gmail.com>
2025-06-24 01:13:04 +0400

    add package and create component for svg files

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,10 @@
+import BrandIcon from "../assets/Brandicon.svg";
+import Icon from "../components/UI/Icon";
+
 export default function HomePage() {
-  return <div className="text-red-500">Home Page</div>;
+  return (
+    <div>
+      <Icon icon={BrandIcon} size={41} />
+    </div>
+  );
 }
